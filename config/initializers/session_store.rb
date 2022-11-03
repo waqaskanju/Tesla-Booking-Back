bc55177a6aec16f801c49eb3fc652117f62d4086
waqas ahmad <waqaskanju@gmail.com>
2022-11-03 18:40:00 +0500

    add default app

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+if Rails.env == "production"
+    Rails.application.config.session_store :cookie_store,
+                                           key: '_authentication_app'
+else
+        Rails.application.config.session_store :cookie_store,
+        key: '_authentication_app'
+end
---
+++ db/schema.rb
@@ -0,0 +1,52 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2022_10_20_180047) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "cars", force: :cascade do |t|
+    t.string "model"
+    t.string "color"
+    t.string "image"
+    t.string "description"
+    t.integer "range"
+    t.string "motor_type"
+    t.decimal "acceleration_time"
+    t.decimal "booking_price", precision: 22, scale: 2
+    t.integer "booking_duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.date "booking_date"
+    t.bigint "user_id", null: false
+    t.bigint "car_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_reservations_on_car_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "admin", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "reservations", "cars"
+  add_foreign_key "reservations", "users"
+end
