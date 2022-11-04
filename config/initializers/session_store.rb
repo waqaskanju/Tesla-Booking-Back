1b7732866aceec5539e7080a13f2a3367eb6cab7
Wajahat Ahmad <48358590+WjahatAhmad@users.noreply.github.com>
2022-11-04 19:01:33 +0500

    Update session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 if Rails.env == "production"
     Rails.application.config.session_store :cookie_store,
-                                           key: '_authentication_app'
+                                           key: '_authentication_app',
+                                           domian: "https://w-tesla-front.herokuapp.com/"
 else
         Rails.application.config.session_store :cookie_store,
         key: '_authentication_app'
