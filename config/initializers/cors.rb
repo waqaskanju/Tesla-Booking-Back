0fa76fcb9218b4d003389b282f9f71767b9bb89c
Wajahat Ahmad <48358590+WjahatAhmad@users.noreply.github.com>
2022-11-04 19:00:27 +0500

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'https://w-tesla-front.herokuapp.com:443'
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head],
